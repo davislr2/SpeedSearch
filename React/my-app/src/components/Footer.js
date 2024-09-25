74a28045141e9d85116e5116f8ddef5d3f6daa43
“davislr2” <davislr2@appstate.edu>
2024-09-25 13:26:57 -0400

    added border to footer and navbar

--- React/my-app/src/components/Footer.js
+++ React/my-app/src/components/Footer.js
@@ -7,9 +7,9 @@ function Footer() {
             <div className="additional-resources">
                 <h2>Additional Resources</h2>
                 <ul>
-                    <li><a href="https://www.formula1.com/">Formula One</a></li>
-                    <li><a href="https://www.fiaformula2.com/">Formula Two</a></li>
-                    <li><a href="https://www.fiaformula3.com/">Formula Three</a></li>
+                    <li><a href="https://www.formula1.com/"     target="_blank">Formula One</a></li>
+                    <li><a href="https://www.fiaformula2.com/"  target="_blank">Formula Two</a></li>
+                    <li><a href="https://www.fiaformula3.com/"  target="_blank">Formula Three</a></li>
                 </ul>
             </div>
         </footer>
