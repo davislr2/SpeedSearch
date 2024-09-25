bbe9da55db77a07ed9fb4c5f36d219783e119f59
“davislr2” <davislr2@appstate.edu>
2024-09-25 11:12:13 -0400

    started the F1 page, and the header link points to it now.

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar.js";
 import Searchbar from "./components/Searchbar.js";
 import Footer from "./components/Footer.js";
+import F1 from "./pages/F1.js";
+
+function AppLayout({ children }) {
+  const location = useLocation();
+  
+  // Define paths where you don't want the Navbar or Footer to appear
+  const hideNavbarFooterPaths = ["/f1"];
+  const currentPath = location.pathname.replace(/\/$/, "");
+
+  return (
+    <div className="App">
+      {/* Conditionally render Navbar and Footer */}
+      {!hideNavbarFooterPaths.includes(currentPath) && <Navbar />}
+      
+      {children}  {/* This will render the Routes */}
+      
+      {!hideNavbarFooterPaths.includes(currentPath) && <Footer />}
+    </div>
+  );
+}
+
 
 class App extends React.Component {
     constructor(props) {
@@ -19,17 +41,25 @@ callAPI() {
   componentDidMount() {
     this.callAPI();
 }
-  render() {
-    return (
-      <div className="App">
-        <Navbar />
-        <Searchbar />
-        <Footer />
-        <header className="App-header">
-          <p>{this.state.apiResponse}</p>
-        </header>
-      </div>
-    );
-  }
+render() {
+  return (
+      <Router>
+        <AppLayout>  {/* Wrap Routes with AppLayout for conditional rendering */}
+          <Routes>
+            <Route path="/" element={
+              <>
+                <Searchbar />
+                <header className="App-header">
+                  <p>{this.state.apiResponse}</p>
+                </header>
+              </>
+            } />
+            
+            <Route path="/f1" element={<F1 />} />  {/* Define the F1 route */}
+          </Routes>
+        </AppLayout>
+      </Router>
+  );
+}
 }
 export default App;
---
+++ React/my-app/src/pages/F1.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+function F1() {
+    return (
+        <div className="seasons">
+            <h2>Seasons</h2>
+            <table className = "seasons-table">
+                <tr>
+                    <td>1950</td> <td>1951</td> <td>1952</td> <td>1953</td> <td>1954</td> 
+                    <td>1955</td> <td>1956</td> <td>1957</td> <td>1958</td> <td>1959</td> 
+                    <td>1960</td> <td>1961</td> <td>1962</td> <td>1963</td> <td>1964</td>
+                </tr>
+                <tr>
+                    <td>1965</td> <td>1966</td> <td>1967</td> <td>1968</td> <td>1969</td> 
+                    <td>1970</td> <td>1971</td> <td>1972</td> <td>1973</td> <td>1974</td> 
+                    <td>1975</td> <td>1976</td> <td>1977</td> <td>1978</td> <td>1979</td>
+                </tr>
+                <tr>
+                    <td>1980</td> <td>1981</td> <td>1982</td> <td>1983</td> <td>1984</td> 
+                    <td>1985</td> <td>1986</td> <td>1987</td> <td>1988</td> <td>1989</td> 
+                    <td>1990</td> <td>1991</td> <td>1992</td> <td>1993</td> <td>1994</td>
+                </tr>
+                <tr>
+                    <td>1995</td> <td>1996</td> <td>1997</td> <td>1998</td> <td>1999</td> 
+                    <td>2000</td> <td>2001</td> <td>2002</td> <td>2003</td> <td>2004</td> 
+                    <td>2005</td> <td>2006</td> <td>2007</td> <td>2008</td> <td>2009</td>
+                </tr>
+                <tr>
+                    <td>2010</td> <td>2011</td> <td>2012</td> <td>2013</td> <td>2014</td> 
+                    <td>2015</td> <td>2016</td> <td>2017</td> <td>2018</td> <td>2019</td> 
+                    <td>2020</td> <td>2021</td> <td>2022</td> <td>2023</td> <td>2024</td>
+                </tr>
+            </table>
+        </div>
+    );
+}
+
+export default F1;
