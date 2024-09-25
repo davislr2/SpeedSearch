c6006db95b7c1afcd2072fd5b1a4ae0b318dc187
“davislr2” <davislr2@appstate.edu>
2024-09-25 13:19:53 -0400

    Revert "started the F1 page, and the header link points to it now."
    This reverts commit bbe9da55db77a07ed9fb4c5f36d219783e119f59.

--- React/my-app/src/components/Navbar.js
+++ React/my-app/src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import '../styles/Navbar.css'
+
 function Navbar() {
     return (
         <nav className="navbar">
             <ul className="nav-links">
-                <li><a href="/f1">Formula One</a></li>
+                <li><a href="/">Formula One</a></li>
                 <li><a href="/">Formula Two</a></li>
                 <li><a href="/">Formula Three</a></li>
             </ul>
