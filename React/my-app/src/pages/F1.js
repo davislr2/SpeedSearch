f19fb92173d51f3f915db183c5922e209d27edda
“davislr2” <davislr2@appstate.edu>
2024-10-08 13:31:56 -0400

    Updated links for seasons in F1/2/3 pages

--- React/my-app/src/pages/F1.js
+++ React/my-app/src/pages/F1.js
@@ -8,20 +8,45 @@ function F1() {
             <h1>Formula One</h1>
             <h3>Seasons</h3>
             <table className='seasons-table'>
+                // 1950-1964
                 <tr>
-                    <td><Link className='link' to='/F1/Seasons/1950'>1950</Link></td><td>1951</td><td>1952</td><td>1953</td><td>1954</td><td>1955</td><td>1956</td><td>1957</td><td>1958</td><td>1959</td><td>1960</td><td>1961</td><td>1962</td><td>1963</td><td>1964</td>
+                    <td><Link className='link' to='/F1/Seasons/1950'>1950</Link></td><td><Link className='link' to='/F1/Seasons/1951'>1951</Link></td><td><Link className='link' to='/F1/Seasons/1952'>1952</Link></td>
+                    <td><Link className='link' to='/F1/Seasons/1953'>1953</Link></td><td><Link className='link' to='/F1/Seasons/1954'>1954</Link></td><td><Link className='link' to='/F1/Seasons/1955'>1955</Link></td>
+                    <td><Link className='link' to='/F1/Seasons/1956'>1956</Link></td><td><Link className='link' to='/F1/Seasons/1957'>1957</Link></td><td><Link className='link' to='/F1/Seasons/1958'>1958</Link></td>
+                    <td><Link className='link' to='/F1/Seasons/1959'>1959</Link></td><td><Link className='link' to='/F1/Seasons/1960'>1960</Link></td><td><Link className='link' to='/F1/Seasons/1961'>1961</Link></td>
+                    <td><Link className='link' to='/F1/Seasons/1962'>1962</Link></td><td><Link className='link' to='/F1/Seasons/1963'>1963</Link></td><td><Link className='link' to='/F1/Seasons/1964'>1964</Link></td>
                 </tr>
+                // 1965-1979
                 <tr>
-                    <td>1965</td><td>1966</td><td>1967</td><td>1968</td><td>1969</td><td>1970</td><td>1971</td><td>1972</td><td>1973</td><td>1974</td><td>1975</td><td>1976</td><td>1977</td><td>1978</td><td>1979</td>
+                    <td><Link className='link' to='/F1/Seasons/1965'>1965</Link></td><td><Link className='link' to='/F1/Seasons/1966'>1966</Link></td><td><Link className='link' to='/F1/Seasons/1967'>1967</Link></td>
+                    <td><Link className='link' to='/F1/Seasons/1968'>1968</Link></td><td><Link className='link' to='/F1/Seasons/1969'>1969</Link></td><td><Link className='link' to='/F1/Seasons/1970'>1970</Link></td>
+                    <td><Link className='link' to='/F1/Seasons/1971'>1971</Link></td><td><Link className='link' to='/F1/Seasons/1972'>1972</Link></td><td><Link className='link' to='/F1/Seasons/1973'>1973</Link></td>
+                    <td><Link className='link' to='/F1/Seasons/1974'>1974</Link></td><td><Link className='link' to='/F1/Seasons/1975'>1975</Link></td><td><Link className='link' to='/F1/Seasons/1976'>1976</Link></td>
+                    <td><Link className='link' to='/F1/Seasons/1977'>1977</Link></td><td><Link className='link' to='/F1/Seasons/1978'>1978</Link></td><td><Link className='link' to='/F1/Seasons/1979'>1979</Link></td>
                 </tr>
+                // 1980-1994
                 <tr>
-                    <td>1980</td><td>1981</td><td>1982</td><td>1983</td><td>1984</td><td>1985</td><td>1986</td><td>1987</td><td>1988</td><td>1989</td><td>1990</td><td>1991</td><td>1992</td><td>1993</td><td>1994</td>
+                    <td><Link className='link' to='/F1/Seasons/1980'>1980</Link></td><td><Link className='link' to='/F1/Seasons/1981'>1981</Link></td><td><Link className='link' to='/F1/Seasons/1982'>1982</Link></td>
+                    <td><Link className='link' to='/F1/Seasons/1983'>1983</Link></td><td><Link className='link' to='/F1/Seasons/1984'>1984</Link></td><td><Link className='link' to='/F1/Seasons/1985'>1985</Link></td>
+                    <td><Link className='link' to='/F1/Seasons/1986'>1986</Link></td><td><Link className='link' to='/F1/Seasons/1987'>1987</Link></td><td><Link className='link' to='/F1/Seasons/1988'>1988</Link></td>
+                    <td><Link className='link' to='/F1/Seasons/1989'>1989</Link></td><td><Link className='link' to='/F1/Seasons/1990'>1990</Link></td><td><Link className='link' to='/F1/Seasons/1991'>1991</Link></td>
+                    <td><Link className='link' to='/F1/Seasons/1992'>1992</Link></td><td><Link className='link' to='/F1/Seasons/1993'>1993</Link></td><td><Link className='link' to='/F1/Seasons/1994'>1994</Link></td>
                 </tr>
+                // 1995-2009
                 <tr>
-                    <td>1995</td><td>1996</td><td>1997</td><td>1998</td><td>1999</td><td>2000</td><td>2001</td><td>2002</td><td>2003</td><td>2004</td><td>2005</td><td>2006</td><td>2007</td><td>2008</td><td>2009</td>
+                    <td><Link className='link' to='/F1/Seasons/1995'>1995</Link></td><td><Link className='link' to='/F1/Seasons/1996'>1996</Link></td><td><Link className='link' to='/F1/Seasons/1997'>1997</Link></td>
+                    <td><Link className='link' to='/F1/Seasons/1998'>1998</Link></td><td><Link className='link' to='/F1/Seasons/1999'>1999</Link></td><td><Link className='link' to='/F1/Seasons/2000'>2000</Link></td>
+                    <td><Link className='link' to='/F1/Seasons/2001'>2001</Link></td><td><Link className='link' to='/F1/Seasons/2002'>2002</Link></td><td><Link className='link' to='/F1/Seasons/2003'>2003</Link></td>
+                    <td><Link className='link' to='/F1/Seasons/2004'>2004</Link></td><td><Link className='link' to='/F1/Seasons/2005'>2005</Link></td><td><Link className='link' to='/F1/Seasons/2006'>2006</Link></td>
+                    <td><Link className='link' to='/F1/Seasons/2007'>2007</Link></td><td><Link className='link' to='/F1/Seasons/2008'>2008</Link></td><td><Link className='link' to='/F1/Seasons/2009'>2009</Link></td>
                 </tr>
+                // 2010-2024
                 <tr>
-                    <td>2010</td><td>2011</td><td>2012</td><td>2013</td><td>2014</td><td>2015</td><td>2016</td><td>2017</td><td>2018</td><td>2019</td><td>2020</td><td>2021</td><td>2022</td><td>2023</td><td>2024</td>
+                    <td><Link className='link' to='/F1/Seasons/2010'>2010</Link></td><td><Link className='link' to='/F1/Seasons/2011'>2011</Link></td><td><Link className='link' to='/F1/Seasons/2012'>2012</Link></td>
+                    <td><Link className='link' to='/F1/Seasons/2013'>2013</Link></td><td><Link className='link' to='/F1/Seasons/2014'>2014</Link></td><td><Link className='link' to='/F1/Seasons/2015'>2015</Link></td>
+                    <td><Link className='link' to='/F1/Seasons/2016'>2016</Link></td><td><Link className='link' to='/F1/Seasons/2017'>2017</Link></td><td><Link className='link' to='/F1/Seasons/2018'>2018</Link></td>
+                    <td><Link className='link' to='/F1/Seasons/2019'>2019</Link></td><td><Link className='link' to='/F1/Seasons/2020'>2020</Link></td><td><Link className='link' to='/F1/Seasons/2021'>2021</Link></td>
+                    <td><Link className='link' to='/F1/Seasons/2022'>2022</Link></td><td><Link className='link' to='/F1/Seasons/2023'>2023</Link></td><td><Link className='link' to='/F1/Seasons/2024'>2024</Link></td>
                 </tr>
             </table>
 
