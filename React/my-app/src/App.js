d018854d7b6854964487e63cb11c31f584df2e5e
“davislr2” <davislr2@appstate.edu>
2024-10-16 18:13:38 -0400

    got started on the search engine

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -9,28 +9,21 @@ import Season from './pages/Season.js';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import {Switch} from 'react-router-dom';
-/*
-class App extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = { apiResponse: "" };
-    }
 
-callAPI() {
-    fetch("http://127.0.0.1:5000/example")
-        .then((res) => res.text())
-        .then((res) => this.setState({ apiResponse: res }))
-        .catch((err) => console.error("API fetch error: ", err));
-  };
 
-  componentDidMount() {
-    this.callAPI();
-}
-*/
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      apiResponse: ""
+    };
+  }
+
+
   componentDidMount() {
     document.title = "SpeedSearch";
   }
+
   render() {
     return (
       <Router>
