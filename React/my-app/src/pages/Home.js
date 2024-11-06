a98cdce0546b7c7e77890bcfbb7e49bd447920df
“davislr2” <davislr2@appstate.edu>
2024-11-06 17:31:56 -0500

    added a banner to the top of the homepage.

--- React/my-app/src/pages/Home.js
+++ React/my-app/src/pages/Home.js
@@ -3,11 +3,17 @@ import '../styles/Home.css';
 import {Link} from 'react-router-dom';
 import Navbar from "../components/Navbar.js";
 import Searchbar from "../components/Searchbar.js";
+import checkeredFlag from '../images/checkered_flag.jpg';
 
 
 function Home() {
     return (
         <div className="Home">
+            <div className="banner">
+                <img className="banner-image-left" src={checkeredFlag} alt="checkered flag" />
+                <h1 className="banner-text">SpeedSearch</h1>
+                <img className="banner-image-right" src={checkeredFlag} alt="checkered flag" />
+            </div>
             <Navbar />
             <Searchbar />
             <Link className="query-forms-link" to="/query_forms">Query Forms</Link>
